bcc6f593a0f7655e7c5a5e6235c69679660c39da
teranox <kent.mima@gmail.com>
2023-12-08 01:29:38 +0900

    apitest

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var dogRouter = require('./routes/dog');
 
 var app = express();
 
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/cat', catRouter);
+app.use('/dog', dogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
